683495c2ec07a372510aef53496a4e46987e3ac1
Shubham Parmar <shubhamparmar62480@gmail.com>
2026-05-16 14:04:04 +0530

    tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,44 @@
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+
+print(T1)
+
+print(T1[0])
+
+print(T1[1:5])
+
+
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+print(T1.count(13))   # same value kitni baar hai
+ 
+
+
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+print(T1.index(56))   # value ka index batata hai
+ 
+
+
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+print(len(T1))    # total elements count
+ 
+
+
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+print(max(T1))    # sabse bada number
+ 
+
+
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+print(min(T1))    # sabse chhota number
+ 
+
+
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+print(sum(T1))    # total sum
+ 
+
+
+T1 = (11 ,56 ,23 ,13 ,5 ,2 ,8 ,18 ,13)
+print(23 in T1)   # value tuple me hai ya nahi
+ 
+
+
